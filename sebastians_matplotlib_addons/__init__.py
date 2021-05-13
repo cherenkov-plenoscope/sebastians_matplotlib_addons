134316bf57d00739b812955480221524bdd77994
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-05-13 13:57:24 +0200

    add histogram

--- sebastians_matplotlib_addons/__init__.py
+++ sebastians_matplotlib_addons/__init__.py
@@ -1,8 +1,9 @@
+import numpy as np
 import matplotlib
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-
+import matplotlib.colors as plt_colors
 
 FIGURE_16_9 = {"rows": 1080, "cols": 1920, "fontsize": 1}
 FIGURE_4_3 = {"rows": 1080, "cols": 1440, "fontsize": 1}
@@ -16,6 +17,10 @@ def figure(style=FIGURE_16_9, dpi=240):
     return plt.figure(
         figsize=(width_inch / scale, height_inch / scale), dpi=dpi * scale
     )
+
+
+def close_figure(fig):
+    plt.close(fig)
 
 
 AXES_BLANK = {"spines": [], "axes": [], "grid": False}
@@ -42,6 +47,47 @@ def add_axes(fig, span, style=AXES_MINIMAL):
     else:
         ax.axes.get_yaxis().set_visible(False)
 
-    if style["grig"]:
-        ax.grid(color="k", linestyle="-", linewidth=0.66, alpha=0.1)
+    if style["grid"]:
+        ax_add_grid(ax)
     return ax
+
+
+def ax_add_grid(ax):
+    ax.grid(color="k", linestyle="-", linewidth=0.66, alpha=0.1)
+
+
+def ax_add_histogram(
+    ax,
+    bin_edges,
+    bincounts,
+    linestyle="-",
+    linecolor="k",
+    linealpha=1.0,
+    bincounts_upper=None,
+    bincounts_lower=None,
+    face_color=None,
+    face_alpha=None,
+):
+    assert bin_edges.shape[0] == bincounts.shape[0] + 1
+    for i, bincount in enumerate(bincounts):
+        if bincount > 0:
+            ax.plot(
+                [bin_edges[i], bin_edges[i + 1]],
+                [bincount, bincount],
+                linestyle=linestyle,
+                color=linecolor,
+                alpha=linealpha,
+            )
+        if bincounts_upper is not None and bincounts_lower is not None:
+            both_nan = np.isnan(bincounts_upper[i]) and np.isnan(
+                bincounts_lower[i]
+            )
+            if not both_nan:
+                ax.fill_between(
+                    x=[bin_edges[i], bin_edges[i + 1]],
+                    y1=[bincounts_lower[i], bincounts_lower[i]],
+                    y2=[bincounts_upper[i], bincounts_upper[i]],
+                    color=face_color,
+                    alpha=face_alpha,
+                    edgecolor="none",
+                )
