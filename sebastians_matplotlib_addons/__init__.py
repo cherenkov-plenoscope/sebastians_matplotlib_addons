e1bc277cc36bf3fd08ebaac444cceeaba9081d33
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-06-17 12:22:52 +0200

    make videos from image-sequence

--- sebastians_matplotlib_addons/__init__.py
+++ sebastians_matplotlib_addons/__init__.py
@@ -8,7 +8,7 @@ import matplotlib.colors as plt_colors
 FIGURE_16_9 = {"rows": 1080, "cols": 1920, "fontsize": 1}
 FIGURE_4_3 = {"rows": 1080, "cols": 1440, "fontsize": 1}
 FIGURE_1_1 = {"rows": 1080, "cols": 1080, "fontsize": 1}
-FIGURE_2_1 = {"rows": 2*1080, "cols": 1080, "fontsize": 1}
+FIGURE_2_1 = {"rows": 2 * 1080, "cols": 1080, "fontsize": 1}
 
 
 def figure(style=FIGURE_16_9, dpi=240):
@@ -125,3 +125,54 @@ def ax_add_histogram(
                     alpha=face_alpha,
                     edgecolor="none",
                 )
+
+
+def write_video_from_image_slices(
+    image_sequence_wildcard_path, output_path, frames_per_second=30, threads=1,
+):
+    """
+    Writes an h264 video.mov from an image-sequence
+
+    Parameters
+    ----------
+    image_sequence_wildcard_path : str, path
+            Path to the imaege-sequence using a six-digit wildcard '%06d'.
+    output_path : str, path
+            Path to write the final movie to.
+    frames_per_second : int
+            Number of frames per second in video.
+    threads : int
+            The number of compute-threads to be used.
+    """
+    outpath = os.path.splitext(output_path)[0]
+    o_path = outpath + ".stdour"
+    e_path = outpath + ".stderr"
+    v_path = outpath + ".mov"
+
+    with open(o_path, "w") as stdout, open(e_path, "w") as stderr:
+        rc = subprocess.call(
+            [
+                "ffmpeg",
+                "-y",  # force overwriting of existing output file
+                "-framerate",
+                str(int(frames_per_second)),
+                "-f",
+                "image2",
+                "-i",
+                image_sequence_wildcard_path,
+                "-c:v",
+                "h264",
+                # '-s', '1920x1080', # sample images down to FullHD 1080p
+                "-crf",
+                "23",  # high quality 0 (best) to 53 (worst)
+                "-crf_max",
+                "25",  # worst quality allowed
+                "-threads",
+                str(threads),
+                v_path,
+            ],
+            stdout=stdout,
+            stderr=stderr,
+        )
+
+    return rc
